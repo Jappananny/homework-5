b139bb5ad7c00deb7a1f013fcbbc7c98707e41db
Jappananny <Jappananny@gmail.com>
2022-10-13 21:56:12 +0300

    ДЗ-5

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         }
         //Задача 3
         System.out.println("Задача 3");
-        for (int i3 = 0; i3 < 17; i3+=2) {
+        for (int i3 = 0; i3 < 17; i3 += 2) {
             System.out.println("Котиков у бабы Гали стало " + i3);
         }
         //Задача 4
@@ -20,6 +20,10 @@ public class Main {
         for (int i4 = 10; i4 > -11; i4--) {
             System.out.println("Котиков у бабы Гали стало " + i4);
         }
+        //Задача 5
+        System.out.println("Задача 5");
+        for (int i5 = 1904; i5 < 2097; i5 = i5 + 4) {
+            System.out.println(i5 + " год является високосным");
+        }
     }
-
 }
