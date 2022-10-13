cd66768b36b3e650b4845c8ea913cec8958e0174
Jappananny <Jappananny@gmail.com>
2022-10-13 21:31:26 +0300

    ДЗ-2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Задача 1");
         for (int i = 1; i < 11; i++) {
-            System.out.println("Котиков у бабы гали стало " + i);
+            System.out.println("Котиков у бабы Гали стало " + i);
+        }
+        //Задача 2
+        System.out.println("Задача 2");
+        for (int i2 = 10; i2 > 0; i2--) {
+            System.out.println("Котиков у бабы Гали стало " + i2);
+
         }
 
 
